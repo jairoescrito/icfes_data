fe6e64d5b19cfd6c7c2409509da372a38be8fa9a
jairoescrito <jairoescrito@gmail.com>
2024-01-04 15:10:25 -0500

    Commit desde Rstudio

--- 02_Exploracion01.py
+++ 02_Exploracion01.py
@@ -7,78 +7,68 @@ Created on Sat Dec 30 21:35:27 2023
 """
 
 import pandas as pd
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+import plotly.express as px
+import plotly.io as pio
+pio.renderers.default='browser'
 
-# Dataset estudiantes
+##### Dataset estudiantes #####
 
+# Carga de datos
 estudiantes = pd.read_csv('Dataset/estudiantes.csv',sep =',', low_memory=False)
 ubicaciones = pd.read_csv('Dataset/ubicacion.csv',sep =',', low_memory=False)
+##
 # Modificación del tipo de identificación para facilitar su entendimiento
 estudiantes.Tipo_Documento = estudiantes.Tipo_Documento.replace(['CC', 'CE', 'CR', 'PC', 'PE', 'TI', 'NES', 
                                                                  'PEP', 'RC', 'CCB','NIP', 'NUIP', 'V'],
                                                                 ["Cedula_Ciudadania","Cedula_Extranjeria","Otro","Otro","Otro",
                                                                  "Tarjeta_Identidad","Otro","Permiso_Esp_Permanencia","Registro_Civil",
                                                                  "Otro","Numero_Id_Personal","Numero_Unico_Id_Personal","Otro"])
+##
+# Revisión del tipos de datos
 estudiantes.info()
-# Agregar fechas de presentación de las pruebas 
-periodos = estudiantes.Periodo.unique().tolist()
-fechas = ['07/11/2020','11/08/2019','10/03/2019','25/02/2018','12/08/2018','18/10/2020']
-cortes = pd.DataFrame({'Periodo':periodos, 'Fecha_Prueba':fechas})
-cortes.info()
+##
+# Inclusión fechas de presentación de las pruebas 
+periodos = estudiantes.Periodo.unique().tolist() # Crear una lista de los periodos de presentación de las pruebas
+fechas = ['07/11/2020','11/08/2019','10/03/2019','25/02/2018','12/08/2018','18/10/2020'] # Fechas de presentación de la prueba por periodo consultadas en internet
+cortes = pd.DataFrame({'Periodo':periodos, 'Fecha_Prueba':fechas}) # Data frame que relaciona las fechas con los periodos
+cortes.info() # Verificar la estructura de este DF temporal
 estudiantes = pd.merge(estudiantes,cortes,on='Periodo',how='left') # Incluir en el dataset de estudiantes la fecha de presentación de examen
-del(cortes,fechas,periodos)
-
-
-# Calcular edades de los estudiantes al momento de presentación de la prueba
+del(cortes,fechas,periodos) # Eliminar variables tempporales
+##
+# Calculo de edades de los estudiantes al momento de presentación de la prueba
 estudiantes.Fecha_Nacimiento.isnull().sum() # Las fechas de naciomiento no tienen nulos
 estudiantes.Fecha_Prueba.isnull().sum() # Las fechas de pruebas no tienen datos nulos
-
-
-
-
-
-
-
-
-
-
-
-# Exploración de datos de estudiantes
-
-# Reemplazo de  siglas para facilitar entendimiento
-
-
-# Dataset con la inclusión de los nombres de las ubicaciones geográficas
-estudiantes['Cod_Municipio'] = estudiantes['Cod_Municipio'].astype('category')
-ubicacion['Cod_Municipio'] = ubicacion['Cod_Municipio'].astype('category')
-
-data_est = pd.merge(estudiantes,ubicacion, on = 'Cod_Municipio', how = 'left')
-ubicacion.info()
+estudiantes.Fecha_Nacimiento = pd.DataFrame(datetime.strptime(x, "%d/%m/%Y") for x in estudiantes.Fecha_Nacimiento) # Convertir fecha a datetime
+estudiantes.Fecha_Prueba = pd.DataFrame(datetime.strptime(x,"%d/%m/%Y") for x in estudiantes.Fecha_Prueba) # Convertir a fecha datetime
+estudiantes['Edad'] = pd.DataFrame(relativedelta(estudiantes.Fecha_Prueba[i],estudiantes.Fecha_Nacimiento[i]).years for i in range(len(estudiantes))) # Calculo de la edad
+# Revisión del tipos de datos luego de los primeros ajustes
 estudiantes.info()
-data_est.isnull().sum()
+##
+# Verificación de missing values
+estudiantes.isna().sum()
+##
+# Revisión de los datos de edades
+round(estudiantes.Edad.describe(),2)
+fig = px.box(estudiantes.Edad, y="Edad")
+fig
 
 
 
 
 
 
-# Dataset con la inclusión de la información del periodo de presentación de la prueba
-data_est = estudiantes.merge(resultados.iloc[:,[0,3]], on = 'Consecutivo_Est', how = 'left')
-temp = data_est[data_est['Region'].isna()]
-temp = pd.DataFrame(data_est.columns) # dataframe temporal para acceder al nombre de las variables
-data_est = data_est.iloc[:,[15,0,1,2,3,4,5,6,9,10,11,13,14]] # Reducir el conjunto de datos a columnas requeridas
-# Creación de data con subgrupos de conteo de estudiantes por cada dimensión
-temp = pd.DataFrame(data_est.columns)# dataframe temporal para acceder al nombre de las variables
-data_est_group_I = data_est.groupby(['Periodo','Tipo_Documento',
-                                     'Nacionalidad','Genero']).count().iloc[:,0].reset_index() # Agrupación, selección de columnas y eliminación de indices
+
+
+pd.to_datetime(estudiantes.Fecha_Nacimiento, format='mixed')
+estudiantes.Fecha_Nacimiento.astype('datetime32')
+estudiantes.Edad.dtype
 
 
 
 
 
-
-data_est_group_II = data_est.groupby(['Periodo','Tipo_Documento',
-                                       'Genero','Region',
-                                       'Departamento','Municipio'])
 
 
 
